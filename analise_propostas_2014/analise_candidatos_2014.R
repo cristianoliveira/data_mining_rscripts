6823ec54b3619425ada4e9aba23758e6aae989ae
Cristian Oliveira <contato@cristianoliveira.com.br>
2014-09-02 21:24:49 -0300

    Análise termos em comum

--- analise_propostas_2014/analise_candidatos_2014.R
+++ analise_propostas_2014/analise_candidatos_2014.R
@@ -5,65 +5,73 @@
 #install.packages("tm", dep=T)
 #install.packages("wordcloud", dep=T)
 #install.packages("Rstem", dep=T)
+install.packages("Rgraphviz", dep=T)
+
 
 library(tm)
 library(wordcloud)
-library(Rstem)
+source("http://bioconductor.org/biocLite.R")
+biocLite("Rgraphviz")
 
 # Cria função para baixar e converter de pdf para text
 getDataFromUrl <- function(pUrlPath){
-                    url <- pUrlPath 
-                    dest <- tempfile(fileext = ".pdf")
-                    download.file(url, dest, mode = "wb") 
-                    
-                    system(paste("pdftotext ",dest),wait = T)
-                    
-                    filetxt <- sub(".pdf", ".txt", dest)
-                    
-                    return(readLines(filetxt))
+  url  <- pUrlPath 
+  dest <- tempfile(fileext = ".pdf")
+  download.file(url, dest, mode = "wb")
+  system(paste("pdftotext ",dest),wait = T)
+  filetxt <- sub(".pdf", ".txt", dest)
+  pureText = paste(readLines(filetxt),collapse=" ")
+  
+  pureText
+  
+  return(pureText)
 }
 
-analisaProposta <- function(pText){
+analisaPropostas <- function(pPropostas){
   
-  text = tolower(pText)
+  otherStopWords = c("\f","\\f","MIOLO SEMDiretrizes FULL.indd","políticas","política","governo","governos","milhões","mil","anos", "governos", "governo")
+  propostasLow = tolower(pPropostas)
   
-  cleanedText <- removeWords(text, c("\\f", stopwords('pt-br')))
+  propostasCleaned <- removeWords(propostasLow, c("\\f", stopwords('pt-br')))
+  propostasCleaned <- removeWords(propostasCleaned, otherStopWords)
   
-  corpus <- Corpus(VectorSource(cleanedText))
+  corpus <- Corpus(VectorSource(propostasCleaned))
   corpus <- tm_map(corpus, removePunctuation)
   corpus <- tm_map(corpus, removeNumbers)
   tdm    <- TermDocumentMatrix(corpus)
   m      <- as.matrix(tdm)
   d      <- data.frame(freq = sort(rowSums(m), decreasing = TRUE))
   
-  print(pText)
-  
-  return(d)
+  as.TermDocumentMatrix(m, weighting = weightTf)
   
 }
 
 # Proposta Aecio
-proposta = getDataFromUrl("http://divulgacand2014.tse.jus.br/divulga-cand-2014/proposta/eleicao/2014/idEleicao/143/UE/BR/candidato/280000000085/idarquivo/229?x=1408666351000280000000085")
-dadosAecio = analisaProposta(proposta)
-topDezAecio = head(dadosAecio,10)
+propostaAecio = getDataFromUrl("http://divulgacand2014.tse.jus.br/divulga-cand-2014/proposta/eleicao/2014/idEleicao/143/UE/BR/candidato/280000000085/idarquivo/229?x=1408666351000280000000085")
+dadosAecio    = analisaProposta(propostaAecio)
+topDezAecio   = head(dadosAecio,10)
 
 #Proposta Dilma
-proposta = getDataFromUrl("http://divulgacand2014.tse.jus.br/divulga-cand-2014/proposta/eleicao/2014/idEleicao/143/UE/BR/candidato/280000000083/idarquivo/194?x=1406931687000280000000083")
-proposta = gsub("milhões","", tolower(proposta))
-proposta = gsub("mil","", tolower(proposta))
-proposta = gsub("anos","", tolower(proposta))
-dadosDilma = analisaProposta(proposta)
-topDezDilma = head(dadosDilma, 10)
+propostaDilma = getDataFromUrl("http://divulgacand2014.tse.jus.br/divulga-cand-2014/proposta/eleicao/2014/idEleicao/143/UE/BR/candidato/280000000083/idarquivo/194?x=1406931687000280000000083")
+dadosDilma    = analisaProposta(propostaDilma)
+topDezDilma   = head(dadosDilma, 10)
 
 #Proposta Marina
-proposta = getDataFromUrl("http://divulgacand2014.tse.jus.br/divulga-cand-2014/proposta/eleicao/2014/idEleicao/143/UE/BR/candidato/280000000063/idarquivo/108?x=1408755983000280000000063")
+propostaMarina = getDataFromUrl("http://divulgacand2014.tse.jus.br/divulga-cand-2014/proposta/eleicao/2014/idEleicao/143/UE/BR/candidato/280000000063/idarquivo/108?x=1408755983000280000000063")
 #tratamento para rodapé
-proposta = gsub("MIOLO SEMDiretrizes FULL.indd", "", proposta)
-dadosMarina = analisaProposta(proposta)
-head(dadosMarina,10)
+#propostaMarina     = gsub(, "", propostaMarina)
+dadosMarina  = analisaProposta(propostaMarina)
+topDezMarina = head(dadosMarina,10)
 
-#todo Implementar avaliação por area
-areas = c("saúde","educação","social","economia")
-for (i in areas){
-  print(i)
-} 
+todasPropostas = c(propostaAecio, propostaDilma, propostaMarina)
+analisePropostas = analisaPropostas(todasPropostas)
+
+# seta o cabeçalho de cada documento no TermDocumentMatrix
+colnames(analisePropostas) <- c("Aecio","Dilma","Marina") 
+
+termosEmComum = removeSparseTerms(analisePropostas, 0.1)
+colnames(termosEmComum) <- c("Aecio","Dilma","Marina") 
+
+inspect(termosEmComum)
+
+plot(termosEmComum)
